ba280acf07c66828aab66d96754e4c9154293993
maximumayo <maxgmayo@gmail.com>
2016-03-30 19:26:55 -0700

    shuffle cards on reset

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
     shuffle();
     $(".card").click(function () {
-        //console.log("clicked");
         card_clicked(this);
     });
-    // reset button
+
+    //reset button
     $('.reset').click(function () {
-        //console.log("Reset button clicked");
+        remove_shuffle();
         shuffle();
         games_played++;
         $(".win").hide();
         reset_stats();
         display_stats();
         $('.card').find('.back').show();
-        //console.log(random_face);
     });
 });
 
@@ -25,32 +24,33 @@ var can_click = true; // flag variable
 var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
-// declare array of card faces
-var card_faces = [
-    "images/acoustic01.jpg",
-    "images/harmonica01.jpg",
-    //"images/piano01.png",
-    //"images/strat01.jpg",
-    //"images/sax01.jpg",
-    //"images/drum01.jpg",
-    //"images/lespaul01.jpg",
-    //"images/trumpet01.jpg",
-    //"images/violin01.jpg",
-    "images/acoustic01.jpg",
-    "images/harmonica01.jpg",
-    //"images/piano01.png",
-    //"images/strat01.jpg",
-    //"images/sax01.jpg",
-    //"images/drum01.jpg",
-    //"images/lespaul01.jpg",
-    //"images/trumpet01.jpg",
-    //"images/violin01.jpg"
-];
-
 var random_face = [];
-var card_faces_length = card_faces.length;
-//random shuffle of card faces
+
 function shuffle() {
+
+    var card_faces = [
+        "images/acoustic01.jpg",
+        "images/harmonica01.jpg",
+        "images/piano01.png",
+        "images/strat01.jpg",
+        "images/sax01.jpg",
+        "images/drum01.jpg",
+        "images/lespaul01.jpg",
+        "images/trumpet01.jpg",
+        "images/violin01.jpg",
+        "images/acoustic01.jpg",
+        "images/harmonica01.jpg",
+        "images/piano01.png",
+        "images/strat01.jpg",
+        "images/sax01.jpg",
+        "images/drum01.jpg",
+        "images/lespaul01.jpg",
+        "images/trumpet01.jpg",
+        "images/violin01.jpg"
+    ];
+    var card_faces_length = card_faces.length;
+
+
     for (var i = 0; i < card_faces_length; i++) {
         var current_length = card_faces.length;
         var num = Math.floor(Math.random() * current_length);
@@ -71,26 +71,18 @@ function card_clicked(element) {
     $(element).find('.back').hide();
     if (first_card_clicked == null) {
         first_card_clicked = $(element);
-        //console.log('first_card_clicked is: ', first_card_clicked);
     }
     else {
         second_card_clicked = $(element);
         attempts++;
-        //console.log("number of attempts: " + attempts);
         if (first_card_clicked.find(".front > img").attr('src') == second_card_clicked.find(".front > img").attr('src')) {
             match_counter++;
-            //console.log("MATCH!!");
-            //console.log("match_counter is: ", match_counter);
             accuracy_check(); // updating accuracy
             display_stats(); // updating stats display
-            //console.log("accuracy is: " + accuracy + "%");
             first_card_clicked = null; //reset variable to allow for more clicks
             second_card_clicked = null; //reset variable to allow for more clicks
-            //console.log("first_card_clicked is: ", first_card_clicked +
-            //    " " + "second_card_clicked is: ", second_card_clicked);
             //checking to see if all matches have been made
             if (match_counter == total_possible_matches) {
-                //console.log("YOU WON!!");
                 $(".win").show();
             }
         }
@@ -98,7 +90,6 @@ function card_clicked(element) {
             can_click = false;
             accuracy_check(); // updating accuracy
             display_stats(); // updating stats display
-            //console.log("accuracy is: " + accuracy + "%");
             //wait 2 seconds to flip when cards do not match
             setTimeout(function () {
                 $(first_card_clicked).find('.back').show();
@@ -110,6 +101,7 @@ function card_clicked(element) {
         }
     }
 }
+
 // stats functions
 function accuracy_check() {
     accuracy = Math.round((match_counter / attempts) * 100);
@@ -126,4 +118,9 @@ function reset_stats() {
     attempts = 0;
     accuracy = 0;
     display_stats();
+}
+
+function remove_shuffle() {
+    $('#game-area div .front').remove();
+    random_face = [];
 }
